1bbf9b0b96f7103f9b7286f5924a044b8f24a8bf
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2026-08-25 10:48:16 -0300

    style: padronizar paleta goldenrod no mobile

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color salmon = Color(0xFFFA8072);
-  static const Color darkSalmon = Color(0xFFE9967A);
-  static const Color lightSalmon = Color(0xFFFFA07A);
-  static const Color coral = Color(0xFFFF7F50);
-  static const Color tomato = Color(0xFFFF6347);
+  static const Color goldenrod = Color(0xFFDAA520);
+  static const Color darkGoldenrod = Color(0xFFB8860B);
   static const Color background = Color(0xFF0F0F0F);
   static const Color surface = Color(0xFF161616);
-  static const Color primaryGold = coral;
+  static const Color primaryGold = goldenrod;
   static const Color textPrimary = Colors.white;
   static const Color textSecondary = Colors.grey;
   static const Color gridLine = Color(0xFF1F1F1F);
@@ -21,6 +18,7 @@ class AppTheme {
     primaryColor: AppColors.primaryGold,
     colorScheme: const ColorScheme.dark(
       primary: AppColors.primaryGold,
+      onPrimary: Colors.black,
       surface: AppColors.surface,
     ),
     fontFamily: 'sans-serif',
